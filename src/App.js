f3193e038d7632f0f761ca9b26bec3e385f102a1
musayann <musayann@octangrp.com>
2021-12-14 20:00:55 +0200

    chore: remove mui theme wrapper

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
 import Header from "./components/Header";
 import Tweets from "./components/Tweets";
-
-
-const theme = createTheme({
-  palette: {
-    background: {
-      default: "rgb(225, 31, 35)",
-    },
-  },
-});
 
 function App() {
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <main>
       <Header />
       <Tweets />
-    </ThemeProvider>
+    </main>
   );
 }
 

1ec0f020218eaea434595e61888b6dc7934158a3
karema arsene <karemarsy@gmail.com>
2021-12-16 21:00:39 +0200

    fix: the images issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,19 +4,20 @@ import SnowImage from "../assets/images/snow.png";
 
 function Gift() {
     return (
-        <div className='w-full about-background'>
-            <div className='py-80 text-center mx-auto'>
+        <div className='w-full about-background relative'>
+            <div className='py-80 text-center mx-auto relative'>
                 <p className='w-3/5 2xl:w-2/6 text-[32px] font-primary mx-auto'>
                    This website aggregates tweets with QR codes of Lightning invoices using the #SecretSatsa hashtag.<br/><br/>
                    To see how you can receive or send a gift yourself check out https://secretsatsa.com/ 
                    or just use Alby to pay an invoice and make someone else happy
                 </p>
             </div>
-            <div className='absolute bottom-0'>
-            <img src={GiftImage} alt="header" className='w-[70%]'/>
-            </div>
+            
             <div className='absolute top-0 right-0'>
-            <img src={SnowImage} alt="header" className='w-[95%]'/>
+            <img src={SnowImage} alt="snow" className='w-[95%]'/>
+            </div>
+            <div className='absolute bottom-0 '>
+            <img src={GiftImage} alt="gift" className='w-[70%]'/>
             </div>
             </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import HeaderImage from "../assets/images/background.png";
 export default function Header() {
   return (
     <>
-    <div className="bg-mandy-red min-h-screen grid pb-[4.5rem]">
-      <div className="w-11/12 mx-auto pt-20 text-center">
+    <div className="bg-mandy-red min-h-screen grid pb-[4.5rem] relative">
+      <div className="w-11/12 mx-auto pt-20 text-center relative">
         <img src={HeaderImage} alt="header" className="w-full h-[89%] object-contain" />
         <p className="text-white text-[32px] font-bold w-2/4 pt-9 mx-auto font-primary leading-tight">
           This holiday season, spend some sats for someone else with Alby!

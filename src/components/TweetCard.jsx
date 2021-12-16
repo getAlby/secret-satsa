a55f635ae765ce47fdcdd6b3b2dc70b6063c6869
musayann <musayann@octangrp.com>
2021-12-16 22:31:21 +0200

    feat: add open link to tweet card

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -5,11 +5,18 @@ export default function TweetCard({ tweet }) {
   return (
     <div className="w-full shadow-sm border-[0.5px] border-solid border-grey-200 rounded-xl flex p-10 items-center">
       <div className="h-20 w-20 rounded-full overflow-hidden flex-none border-4 border-solid border-accent">
-        <img src={tweet.user.profile_image_url} alt={tweet.user.name} className="object-cover w-full h-full" />
+        <img
+          src={tweet.user.profile_image_url}
+          alt={tweet.user.name}
+          className="object-cover w-full h-full"
+        />
       </div>
       <div className="flex-1 px-7">{tweet.text}</div>
       <div className="flex-none flex space-x-2">
-        <button className="py-3 bg-dark-blue px-14 text-white rounded-lg">
+        <button
+          onClick={() => window.open(tweet.href)}
+          className="py-3 bg-dark-blue px-14 text-white rounded-lg"
+        >
           Pay Now
         </button>
         <button className="p-2 bg-white text-dark-blue border-2 border-solid border-dark-blue hover:bg-dark-blue hover:text-white rounded-lg">

1f0eee260a8b584100c23f3be758bfa17abd0370
Morrism1 <morrismontana0@gmail.com>
2021-12-18 03:51:54 +0200

    chore(footer): import footer in the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Tweets from "./components/Tweets";
 import How from "./components/HowSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Tweets />
       <About />
       <How />
+      <Footer />
     </main>
   );
 }
